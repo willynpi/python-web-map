55f1793fb729efbe7912da258ad12c90b4b74d71
Ying <wave.tako@gmail.com>
2017-07-22 00:55:35 +0800

    scrap and display

---
+++ scraping-mount.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import urllib
+# import pandas
+from pyproj import Proj
+
+request = urllib.request.urlopen('http://www.mountaineering.org.tw/tw/index.php/2015-09-13-14-48-49/2015-09-13-15-07-13').read()
+soup = BeautifulSoup(request, "html.parser")
+
+
+myProj = Proj("+proj=tmerc +ellps=GRS67 +lon_0=121 +x_0=250000 +k=0.9999 +units=m +no_defs")
+fs = open('mount-data.txt','w')
+fs.seek(0)
+trs = soup.find_all('tr')
+first = trs[1]
+keys = []
+for td in first.find_all('td'):
+    keys.append(td.text.replace("\n","").replace(" ",""))
+
+fs.write("name,latitude,longitude,altitude\n")
+data = []
+for tr in trs[2::]:
+    d = {}
+    tds = tr.find_all('td')
+    try:
+        name = tds[1].text.replace("\n","").replace(" ","")
+        x = tds[2].text.replace("\n","").replace(" ","")
+        y = tds[3].text.replace("\n","").replace(" ","")
+        longt, lat = myProj(x, y, inverse=True)
+        alt = tds[4].text.replace("\n","").replace(" ","").replace("公尺","")
+        print('%s,%s,%s,%s\n' %(name, lat, longt, alt).encode('utf8'))
+        fs.write('%s,%s,%s,%s\n' %(name, lat, longt, alt))
+    except:
+        pass
+fs.close()
--- taiwan-mount-map.py
+++ taiwan-mount-map.py
@@ -3,15 +3,21 @@ import folium
 from math import floor
 
 mount_map = folium.Map([23.770876, 120.934757], zoom_start=8, tiles='Cartodb Positron')
-data = pandas.read_csv("example.txt")
-lat = list(data["LATITUDE"])
-lon = list(data["LONGITUDE"])
-hgt = list(data["ALTITUDE"])
+data = pandas.read_csv("mount-data.txt")
+lat = list(data["latitude"])
+lon = list(data["longitude"])
+hgt = list(data["altitude"])
+name = list(data["name"])
 
-color = ['#66ccff','#3399ff','#0066ff','#0000ff','#000099','#000066']
-for lat,lon,hgt in zip(lat,lon,hgt):
-    colorPick = floor(hgt/300)
-    popup = str(hgt)+" m"
+color = ["#77FF00","#00DD00","#00AA00","#668800","#BB5500","#886600"]
+for name,lat,lon,hgt in zip(name,lat,lon,hgt):
+    if hgt > 3000 :
+        colorPick = floor((hgt-3000)/300)+1
+    else :
+        colorPick = 0
+    html = "<div style=\"font-size:8px\"><p><strong>"+str(name)+ "</strong></p><p>"+str(hgt)+" m</p></div>"
+    iframe = folium.IFrame(html = html, width=90, height=80)
+    popup = folium.Popup(iframe, max_width=200)
     folium.RegularPolygonMarker([lat, lon, hgt],
                                 popup=popup,
                                 number_of_sides=3,
